de28d404b76a22059fa32c090215c73a4ec2b200
Vervulf <vervulf_code@mail.ru>
2017-11-21 15:19:48 +0300

    Open Image

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,45 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QRect>
+#include <QDesktopWidget>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
+    ui(new Ui::MainWindow),
+    img(new QPixmap()),
+    img_path(new QString()),
+    scrollArea(new QScrollArea())
 {
+    int LISTVIEW_WIDTH = 175;
     ui->setupUi(this);
+    setWindowTitle("JPEG Hero");
+
+    ui->clusters_listview->setGeometry(0,0,0,0);
+    ui->img_label->setGeometry(LISTVIEW_WIDTH,0,0,0);
+
+    QDesktopWidget *dw = new QDesktopWidget();
+    QRect *screenSize = new QRect();
+    *screenSize = dw->availableGeometry(this);
+    ui->img_label->resize(screenSize->width()-LISTVIEW_WIDTH,screenSize->height());
+    ui->clusters_listview->resize(LISTVIEW_WIDTH,screenSize->height());
+
+    QObject::connect(ui->action_Open,SIGNAL(triggered(bool)),this,SLOT(get_img()));
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::get_img()
+{
+    *img_path = QFileDialog::getOpenFileName(this,tr("Open File"),
+                                             last_file_path, tr("JPEG File (*.jpg *.jpeg)"));
+    if (*img_path == NULL)
+        return;
+
+    delete img;
+    img = new QPixmap(*img_path);
+    ui->img_label->setPixmap(*img);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,15 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLabel>
+#include <QPixmap>
+#include <QListView>
+#include <QDir>
+#include <QFile>
+#include <QFileDialog>
+#include <QString>
+#include <QScrollArea>
+
 
 namespace Ui {
 class MainWindow;
@@ -17,6 +26,16 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QPixmap *img;
+    QString *img_path, last_file_path = "C://";
+    QScrollArea *scrollArea;
+
+    //QLabel *img_label;
+    //QListView *clusters_lv;
+
+private slots:
+    void get_img();
+
 };
 
 #endif // MAINWINDOW_H
