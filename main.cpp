c181680dcec75ad5b03b903bc3f28a9cfb524ac5
Vervulf <vervulf_code@mail.ru>
2018-06-03 23:26:27 +0300

    fix libpaths

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    QStringList paths = QCoreApplication::libraryPaths();
+    paths.append(".");
+    QCoreApplication::setLibraryPaths(paths);
     w.get_app_path(a.applicationDirPath());
     w.show();
 
